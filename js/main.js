1dd895e4c49b0fdb73cedb8d87856ff1c1244d5c
duzhongbo <710409599@qq.com>
2015-12-30 15:36:50 +0800

    代码整理

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
+var myApp = angular.module('myApp', []);
+
+// 重置angular的数据输出符号，双层花括号变成双层反斜杠
+myApp.config(function($interpolateProvider) {
+  $interpolateProvider.startSymbol('//');
+  $interpolateProvider.endSymbol('//');
+});
+
+// 定义一个控制器
+myApp.controller('aArticleCtrl', function($scope) {
+    $scope.aArticle = aArticle;
+});
+
 
